ee2465aebcde72fba9a221d0c810bb6c776861ee
praneeth <praneethveleti@gmail.com>
2026-07-04 20:01:49 +0530

    Configure Vercel deployment for Vite app

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+const socketUrl = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
+
+const socket = io(socketUrl, {
   autoConnect: false,
   transports: ['websocket']
 });
